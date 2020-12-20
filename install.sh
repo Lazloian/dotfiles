ec313002803fdfc6d0a6b31e69e2b9ddfdee2d78
Lazloian <henry.silva.999@gmail.com>
2020-12-19 19:45:33 -0800

    added more files and installer script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script creates symbolic links to the dotfiles in this directory
+
+# get this directory
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# nvim
+ln -s ${BASEDIR}/nvim ~/.config/nvim/init.vim
+
+# bashrc
+ln -s ${BASEDIR}/bashrc ~/.bashrc
+
+# ssh
+ln -s ${BASEDIR}/ssh ~/.ssh/config
