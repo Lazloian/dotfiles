aadcb2b41c199631a357a72d20ff454fe6092489
Lazloian <henry.silva.999@gmail.com>
2020-12-19 20:32:49 -0800

    fixed script

--- install.sh
+++ install.sh
@@ -6,13 +6,18 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # nvim (must create .config directory)
+mkdir ~/.config
 mkdir ~/.config/nvim
+rm ~/.config/nvim/init.vim
 ln -s ${BASEDIR}/nvim ~/.config/nvim/init.vim
 
 # bashrc
+rm ~/.bashrc
 ln -s ${BASEDIR}/bashrc ~/.bashrc
 
-# ssh
+# ssh (makes .ssh directory just in case)
+mkdir ~/.ssh
+rm ~/.ssh/config
 ln -s ${BASEDIR}/ssh ~/.ssh/config
 
 # install complete
